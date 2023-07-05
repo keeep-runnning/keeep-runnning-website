b79087d26b5bd383dd65b386e1a25d0989227bad
jmkim <rookie2000g@gmail.com>
2023-07-05 11:59:53 +0900

    hamburger, close icon 추가 및 사용

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import { useMediaQuery, useToggle } from "usehooks-ts";
 import NavLink from "./nav-link";
+import CloseIcon from "./close-icon";
+import HamburgerIcon from "./hamburger-icon";
 
 const navLinks = [
   { name: "Home", path: "/" },
@@ -35,8 +37,16 @@ export default function NavLinks() {
     <nav>
       {/* for mobile. */}
       <div className="md:hidden">
-        <button type="button" onClick={toggle} className="p-2">
-          {isOpen ? "close" : "open"}
+        <button
+          type="button"
+          onClick={toggle}
+          className="rounded bg-stone-100 p-3 text-stone-950 hover:bg-stone-200 dark:bg-stone-900 dark:text-stone-50 dark:hover:bg-stone-800"
+        >
+          {isOpen ? (
+            <CloseIcon className="h-5 w-5" />
+          ) : (
+            <HamburgerIcon className="h-5 w-5" />
+          )}
         </button>
         {isOpen ? (
           <div className="fixed inset-0 top-20 overflow-auto bg-stone-100 px-5 py-4 dark:bg-stone-900">
